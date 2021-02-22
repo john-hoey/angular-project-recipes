603a6bd57d1f3035c7293abe3b53ad85db8fb7a6
Dominick <dominickv1998@gmail.com>
2021-02-22 10:48:51 -0500

    added to service

--- src/app/recipe-brain.service.ts
+++ src/app/recipe-brain.service.ts
@@ -7,15 +7,41 @@ import { Injectable } from '@angular/core';
 export class RecipeBrainService {
   apiId: string = '4c88238c';
   apiKey: string = 'd0f24593bc472cc66c9aaaf866211215';
-  apiQ: string = 'chicken';
+  apiQ: string = '';
   recipeSearchUrl: string = 'https://api.edamam.com/search';
   x: number = 0;
   z: number = 25;
   y: number = 25;
 
+  cuisineType: string = '';
+
   constructor(private http: HttpClient) {}
 
+  cusines: any = [
+    'chinese',
+    'indian',
+    'mexican',
+    'spanish',
+    'caribbean',
+    'american',
+    'greek',
+    'italian',
+    'cajun',
+    'thai',
+    'zanzibari',
+  ];
+
+  randomNumber = (): number => {
+    return Math.floor(Math.random() * this.cusines.length);
+  };
+
+  pickCuisine = (anArray: string): string => {
+    return anArray[this.randomNumber()];
+  };
+  cuisine = this.pickCuisine(this.cusines);
+
   getInitialRecipes = (): any => {
+    let apiQ: string = this.cuisine;
     return this.http.get(this.recipeSearchUrl, {
       params: {
         //anytime we refer to a property of a class we must use this. to begin.
@@ -23,30 +49,32 @@ export class RecipeBrainService {
         app_key: this.apiKey,
         from: this.x.toString(),
         to: this.y.toString(),
-        q: this.apiQ,
+        q: apiQ,
       },
     });
   };
 
   getNextRecipes = (): any => {
+    let apiQ: string = this.cuisine;
     return this.http.get(this.recipeSearchUrl, {
       params: {
         app_id: this.apiId,
         app_key: this.apiKey,
         from: (this.x += this.z).toString(),
         to: (this.y += this.z).toString(),
-        q: this.apiQ,
+        q: apiQ,
       },
     });
   };
   getPreviousRecipes = (): any => {
+    let apiQ: string = this.cuisine;
     return this.http.get(this.recipeSearchUrl, {
       params: {
         app_id: this.apiId,
         app_key: this.apiKey,
         from: (this.x -= this.z).toString(),
         to: (this.y -= this.z).toString(),
-        q: this.apiQ,
+        q: apiQ,
       },
     });
   };
