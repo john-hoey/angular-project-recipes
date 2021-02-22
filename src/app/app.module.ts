8882b1158bb6f57b38d89a1ba8e79efdc9cc0cda
john-hoey <john.t.hoey@gmail.com>
2021-02-22 10:07:20 -0500

    initial api call method and pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,13 +24,10 @@ import { FooterComponent } from './footer/footer.component';
     FavoritesComponent,
     RecipeCardComponent,
     FourohfourComponent,
-    FooterComponent
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/recipe-brain.service.ts
+++ src/app/recipe-brain.service.ts
@@ -1,9 +1,53 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeBrainService {
+  apiId: string = '4c88238c';
+  apiKey: string = 'd0f24593bc472cc66c9aaaf866211215';
+  apiQ: string = 'chicken';
+  recipeSearchUrl: string = 'https://api.edamam.com/search';
+  x: number = 0;
+  z: number = 25;
+  y: number = 25;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getInitialRecipes = (): any => {
+    return this.http.get(this.recipeSearchUrl, {
+      params: {
+        //anytime we refer to a property of a class we must use this. to begin.
+        app_id: this.apiId,
+        app_key: this.apiKey,
+        from: this.x.toString(),
+        to: this.y.toString(),
+        q: this.apiQ,
+      },
+    });
+  };
+
+  getNextRecipes = (): any => {
+    return this.http.get(this.recipeSearchUrl, {
+      params: {
+        app_id: this.apiId,
+        app_key: this.apiKey,
+        from: (this.x += this.z).toString(),
+        to: (this.y += this.z).toString(),
+        q: this.apiQ,
+      },
+    });
+  };
+  getPreviousRecipes = (): any => {
+    return this.http.get(this.recipeSearchUrl, {
+      params: {
+        app_id: this.apiId,
+        app_key: this.apiKey,
+        from: (this.x -= this.z).toString(),
+        to: (this.y -= this.z).toString(),
+        q: this.apiQ,
+      },
+    });
+  };
 }
