6b9ae8ff5fc9740c713c881706191a9a169ffc91
nathan <natedrab@gmail.com>
2021-02-23 11:42:03 -0500

    fourohfour and filter favorites

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { RecipeBrainService } from '../recipe-brain.service';
 
 @Component({
@@ -9,6 +10,8 @@ import { RecipeBrainService } from '../recipe-brain.service';
 export class FavoritesComponent implements OnInit {
   favoriteRecipes: any[] = [];
   recipeService: any;
+  searchTerm: string = '';
+
   constructor(private recipeBrainServe: RecipeBrainService) {}
 
   ngOnInit(): void {
@@ -22,5 +25,20 @@ export class FavoritesComponent implements OnInit {
   onFavorite = (recipe: any): void => {
     this.recipeBrainServe.toggleFavorite(recipe);
     this.getAndSetFavorite();
+  };
+
+  setSearchTerm = (term: string) => {
+    this.searchTerm = term;
+    console.log(term);
+  };
+
+  filterFavorites = (): any[] => {
+    console.log(this.favoriteRecipes);
+    return this.favoriteRecipes.filter((item) => {
+      return item.recipe.label
+        .toLowerCase()
+        .trim()
+        .includes(this.searchTerm.toLowerCase().trim());
+    });
   };
 }
